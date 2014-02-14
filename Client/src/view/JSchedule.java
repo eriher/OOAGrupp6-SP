1ac9d20be6e3f1a9ed01ce9ddd0a2a2f8e07edd3
Magnus <magkal@student.chalmers.se>
2014-02-14 10:38:23 +0100

    JSchedule update

--- Client/src/view/JSchedule.java
+++ Client/src/view/JSchedule.java
@@ -10,15 +10,26 @@ package view;
 import java.util.Observable;
 import java.util.Observer;
 
+import model.Schedule;
+
 public class JSchedule implements Observer {
+	Schedule schedule;	
+	
 	public JSchedule() {
-
+		initPanels();
 	}
-
 	/**
 	 * @param o
+	 * @param arg
 	 */
 	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		if(arg instanceof Schedule)
+			schedule = (Schedule)arg;
+		
+	}
+	public void initPanels(){
+		
 		
 	}
 }
