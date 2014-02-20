833e8d018628140f5b18888531bddc5610059947
Stromner <davidstromner@gmail.com>
2014-02-20 19:23:59 +0100

    Skeleton for Client's FileManagement

--- Client/src/model/FileManagement.java
+++ Client/src/model/FileManagement.java
@@ -1,12 +1,51 @@
+GLÖM INTE ATT ÄNDRA DESC.
 /**
  * Write a description of class FileManagement here.
  * 
- * @author David Stromner
- * @version 2013-02-06
+ * @author 
+ * @version 2013-02-20
  */
 
 package model;
 
-public class FileManagement {
+import java.util.HashMap;
 
+public class FileManagement {
+	private HashMap<String, FILE> fileList; // Contains all opened files
+	
+	public FileManagement(){
+		// Create hashmap
+	}
+	
+	/**
+	 * @param file
+	 */
+	public void openFile(String file){
+		// Add to hashmap
+		// Open
+	}
+	
+	/**
+	 * 
+	 */
+	private void closeFile(){
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	public String readFile(){
+		// Check if file is already open for writing.
+		// Read
+		// Return read data
+	}
+	
+	/**
+	 * 
+	 */
+	public void writeFile(){
+		// Check if file is already open for reading.
+		// Write
+	}
 }
