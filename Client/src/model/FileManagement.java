b4905d3d2c77b0e3df63be73afe6f1f3e3131bfb
Simon <s.planhage@gmail.com>
2014-02-21 09:07:10 +0100

    Schedule, ScheduleHandler, Day samt FileManagement
    Desc

--- Client/src/model/FileManagement.java
+++ Client/src/model/FileManagement.java
@@ -1,75 +1,82 @@
-GLÖM INTE ATT ÄNDRA DESC.
 /**
- * Write a description of class FileManagement here.
+ * Handles file management for reading and saving schedule objects to file
  * 
- * @author 
- * @version 2013-02-20
+ * @author Simon Planhage
+ * @version 2014-02-20
  */
 
 package model;
 
-import java.util.HashMap;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class FileManagement {
-	private HashMap<String, FILE> fileList; // Contains all opened files
-	private static FileManagement fileManagement;
+
 	
-	private FileManagement(){
-		// Create hashmap
-	}
-	
-	public static FileManagement getInstance(){
-		if(fileManagement == null){
-			fileManagement = new FileManagement();
+	/**
+	 * Saves the current list of schedules to a file named personNummer.ser
+	 * Some debug information remains
+	 * @param personNummer 
+	 */
+	public void saveSchedules(int personNummer) {
+		
+		ObjectOutputStream oos = null;
+		FileOutputStream fout = null;
+		try {
+		        fout = new FileOutputStream(personNummer + ".ser", true);
+		        oos = new ObjectOutputStream(fout);
+		        oos.writeObject(ScheduleHandler.scheduleList);
+		        System.out.println("Schedule has been saved");
+		} catch (Exception e) {
+		        e.printStackTrace();
+		} finally {
+				try {
+					oos.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				try {
+					fout.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 		}
-		
-		return fileManagement;
 	}
 	
-	/**
-	 * @param file
-	 */
-	public void openReadFile(String file){
-		// Add to hashmap
-		// Open read
-	}
+	
 	
 	/**
-	 * @param file
+	 * Loads the file with saved Schedule objects
+	 * Some debug information remains
+	 * @param personNummer
 	 */
-	public void openWriteFile(String file){
-		// Add to hashmap
-		// Open write
-	}
-	
-	/**
-	 * 
-	 */
-	private void closeReadFile(){
-		
-	}
-	
-	/**
-	 * 
-	 */
-	private void closeWriteFile(){
-		
-	}
-	
-	/**
-	 * @return
-	 */
-	public String readFile(){
-		// Check if file is already open for writing.
-		// Read
-		// Return read data
-	}
-	
-	/**
-	 * 
-	 */
-	public void writeFile(){
-		// Check if file is already open for reading.
-		// Write
+	@SuppressWarnings("unchecked")
+	public static void loadSchedules(int personNummer) {
+		ObjectInputStream objInStream = null;
+		FileInputStream streamIn = null;
+		 try {
+		        streamIn = new FileInputStream(0 + ".ser");
+		        objInStream = new ObjectInputStream(streamIn);
+				ArrayList<Schedule> readCase = (ArrayList<Schedule>) objInStream.readObject();
+		        ScheduleHandler.scheduleList.addAll(readCase);
+		        System.out.println("Schedule has been loaded");
+
+
+		    } catch (Exception e) {
+
+		        e.printStackTrace();
+		 } finally {
+		        if(objInStream != null){
+		            try {
+						objInStream.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+		         } 
+		 }
 	}
 }
