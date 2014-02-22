d0a94118973084c353edfeede1f3a7f06da7cc32
Stromner <davidstromner@gmail.com>
2014-02-22 10:40:59 +0100

    Fixed compiling errors for FileManagement

--- Client/src/model/FileManagement.java
+++ Client/src/model/FileManagement.java
@@ -10,43 +10,43 @@ package model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-
 public class FileManagement {
-	private static FileManagement fm = null;
+	private static FileManagement fm;
 
-	
-	private FileManagement(){
-		
+	private FileManagement() {
+
 	}
-	
+
 	/**
 	 * Singleton method.
 	 * 
 	 * @return The single instance of the object.
 	 */
 	public static FileManagement getInstance() {
-		if (fm == null) 
+		if (fm == null)
 			fm = new FileManagement();
 		return fm;
 	}
-	
+
 	/**
 	 * Reads one line and returns it as String
+	 * 
 	 * @param file
-	 * Name of the file to be read.
-	 * @return
-	 * First line of file as String
+	 *            Name of the file to be read.
+	 * @return First line of file as String
 	 */
 	public String readLine(String file)
 	{
-		try(BufferedReader br = new BufferedReader(new FileReader(file))){
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line;
 			line = br.readLine();
 			return line;
+			// IOException
 		}catch (Exception e) {
-				e.printStackTrace();
-			}
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
-
 }
