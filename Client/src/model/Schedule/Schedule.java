31ad05b4353ecbd800f7e0d513d8c801622f8a7a
David Strömner <davidstromner@gmail.com>
2014-02-28 12:53:15 +0100

    Updated server

---
+++ Client/src/model/Schedule/Schedule.java
@@ -0,0 +1,25 @@
+/**
+ * Write a description of class Schedule here.
+ * 
+ * @author Simon Planhage
+ * @version 2014-02-25
+ */
+
+package model.Schedule;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Schedule implements Serializable {
+
+	private static final long serialVersionUID = -4841663954805333902L;
+	
+	//Variabler här har inga modifiers (private etc.) så att de kan anropas från samma paket utan att behöva skicka
+	//getters och setters-metoder i varje day objekt till servern.
+	ArrayList<Week> weekList;
+	int personNummer;
+
+	public Schedule() {
+		weekList = new ArrayList<Week>();
+	}
+}
---
+++ Server/src/model/Schedule/Day.java
@@ -0,0 +1,37 @@
+package model.Schedule;
+/**
+ * Write a description of class Day here.
+ * 
+ * @author Simon Planhage
+ * @version 2013-02-28
+ */
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import org.joda.time.*;
+
+public class Day implements Serializable {
+	
+	private static final long serialVersionUID = -7092684252961129250L;
+
+	
+	//Variabler h�r har inga modifiers (private etc.) s� att de kan anropas fr�n samma paket utan att beh�va skicka
+	//getters och setters-metoder i varje day objekt till servern.
+	
+	//Dagens nummer i veckan, b�rjar p� 0, kanske inte beh�vs d� de lagras i ordning i Schema-objektets dayList
+	public int dayNr;
+	ArrayList<DateTime> checkInTime;
+	ArrayList<DateTime> checkOutTime;
+	
+	public Day() {
+		checkInTime = new ArrayList<DateTime>();
+		checkOutTime = new ArrayList<DateTime>();
+	}
+	
+
+	DateTime scheduledInTime;
+	DateTime scheduledOutTime;
+	
+}
